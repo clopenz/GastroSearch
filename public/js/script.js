85b411da7061c5dbb6acb94e88650bc7096b7454
clopenz <clopenz@outlook.com>
2024-11-03 19:57:57 -0800

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,211 @@
+let map;
+let currentController;
+let locations = [];
+const resultsContainer = document.querySelector('.results-container');
+
+document.querySelector('#search-form').addEventListener('submit', (e) => {
+	e.preventDefault();
+	handleFormSubmit('#search-form');
+});
+
+document.querySelector('#search-form-mini').addEventListener('submit', (e) => {
+	e.preventDefault();
+	handleFormSubmit('#search-form-mini');
+});
+
+function handleFormSubmit(formId) {
+	const city = document.querySelector(`${formId} #city`).value;
+	const state = document.querySelector(`${formId} #state`).value;
+	const zip = document.querySelector(`${formId} #zip`).value;
+
+	console.log('City:', city);
+	console.log('State:', state);
+	console.log('Zip:', zip);
+
+	if (!city && !state && !zip) {
+		alert('Please enter a city, state, or zip code.');
+		return;
+	}
+
+	if (formId === '#search-form-mini') {
+		const places = document.querySelector('.places');
+		places.innerHTML = '';
+
+		const loading = document.querySelector('.loading');
+		loading.classList.remove('hidden');
+	}
+
+	convertToLatLng(city, state, zip);
+}
+
+// Show list of places based on the submitted address
+function fetchPlaces(url) {
+	if (currentController) {
+		currentController.abort();
+	}
+
+	currentController = new AbortController();
+	const { signal } = currentController;
+
+	fetch(url, { signal })
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+
+			const places = document.querySelector('.places');
+			places.innerHTML = '';
+
+			// Iterate through places and append them to the list
+			data.forEach((place) => {
+				locations = [...locations, place];
+
+				const li = document.createElement('li');
+				li.innerHTML = `
+
+            <div class="name">${place.name}</div>
+
+				<div class="rating"><i class="fa-solid fa-star"></i>${
+					place.rating ? place.rating : '-'
+				}</div>
+
+            <div class="address">${place.vicinity}</div>
+
+				${
+					place.hours && place.hours.open_now
+						? '<div class="hours open">Open Now</div>'
+						: '<div class="hours closed">Closed</div>'
+				}
+
+				${
+					place.website
+						? '<div class="website"><a href="' +
+						  place.website +
+						  '">Website</a></div>'
+						: ''
+				}
+      `;
+
+				places.appendChild(li);
+			});
+
+			// Check if there's a next_page_token and fetch the next page
+			if (data.next_page_token) {
+				// Wait for the next_page_token to become active (2-5 seconds delay)
+				setTimeout(() => {
+					const nextPageUrl = `/api/places?location=${latLng.lat},${lngLng.lng}&radius=500000&keyword=gastroenterologist&pagetoken=${data.next_page_token}`;
+					fetchPlaces(nextPageUrl);
+				}, 3000);
+			}
+		})
+		.then(() => {
+			showMarkers();
+			const loading = document.querySelector('.loading');
+			loading.classList.add('hidden');
+		})
+		.catch((error) => {
+			if (error.name === 'AbortError') {
+				console.log('Previous request aborted, new request started.');
+			} else {
+				console.error('Error:', error);
+			}
+		});
+}
+
+// Show markers on the map
+function showMarkers() {
+	locations.forEach((location) => {
+		const marker = new google.maps.Marker({
+			position: { lat: location.location.lat, lng: location.location.lng },
+			map: map,
+			title: location.title,
+		});
+	});
+
+	const bounds = new google.maps.LatLngBounds();
+	locations.forEach((location) => {
+		bounds.extend(
+			new google.maps.LatLng(location.location.lat, location.location.lng)
+		);
+	});
+	map.fitBounds(bounds);
+
+	locations = [];
+}
+
+// Convert submitted address to Lat/Lng
+function convertToLatLng(city, state, zip) {
+	const address = `${city ? city : ''}${state ? ', ' + state : ''}${
+		zip ? ', ' + zip : ''
+	}`;
+
+	fetch(`/api/geocode?address=${encodeURIComponent(address)}`)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data.latLng);
+			const { lat, lng } = data.latLng;
+
+			const searchUrl = `/api/places?location=${lat},${lng}&radius=50000&keyword=gastroenterologist+digestive+health+GI+doctor`;
+			fetchPlaces(searchUrl);
+			moveCenterLocation(lat, lng);
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+			alert('Unable to locate. Please enter more information.');
+		});
+}
+
+// Initialize Google Maps
+function initMap() {
+	console.log('Initializing map...');
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat: 39.8283, lng: -98.5795 },
+		zoom: 5,
+		disableDefaultUI: true,
+	});
+}
+
+// Load the Google Maps API script
+function loadGoogleMapsScript() {
+	fetch('/api/maps')
+		.then((response) => response.text())
+		.then((url) => {
+			console.log('Loading Google Maps script...');
+			const script = document.createElement('script');
+			script.src = url;
+			script.async = true;
+			script.defer = true;
+			document.head.appendChild(script);
+		})
+		.catch((error) =>
+			console.error('Error loading Google Maps script:', error)
+		);
+}
+
+function moveCenterLocation(lat, lng) {
+	if (map && typeof map.setCenter === 'function') {
+		console.log('Moving map center...');
+		console.log(lat, lng);
+		// Move the center of the map and set the zoom level
+		map.setCenter({ lat, lng });
+		map.setZoom(12);
+
+		// Hide the home container
+		const homeContainer = document.querySelector('.home-container');
+		homeContainer.style.display = 'none';
+
+		// Remove map background filter
+		const mapContainer = document.querySelector('#map');
+		mapContainer.style.backdropFilter = 'none';
+		mapContainer.style.filter = 'none';
+
+		// Show the results container
+		const resultsContainer = document.querySelector('.results-container');
+		resultsContainer.classList.remove('hidden');
+	} else {
+		console.error('Map not initialized correctly.');
+	}
+}
+
+// Initialization
+// Load the Google Maps API script dynamically
+loadGoogleMapsScript();
---
+++ server.js
@@ -0,0 +1,147 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('Server is running');
+});
+
+// Nearby Search with Pagination Support
+app.get('/api/places', async (req, res) => {
+	const { location, radius, keyword } = req.query;
+	const apiKey = 'AIzaSyD6CnVdH_CGjfZHfnksvGbL69aOw0HRvFw';
+	let allResults = [];
+
+	try {
+		let nextPageToken = null;
+		let hasMorePages = true;
+
+		// Loop through paginated results
+		while (hasMorePages) {
+			const response = await axios.get(
+				`https://maps.googleapis.com/maps/api/place/nearbysearch/json`,
+				{
+					params: {
+						location,
+						radius,
+						keyword,
+						pagetoken: nextPageToken || '',
+						key: apiKey,
+					},
+				}
+			);
+
+			// Accumulate the current page results
+			allResults = [...allResults, ...response.data.results];
+
+			// Check if there is a next_page_token for more results
+			nextPageToken = response.data.next_page_token;
+
+			// If there's a next_page_token, wait before making the next request
+			if (nextPageToken) {
+				await new Promise((resolve) => setTimeout(resolve, 3000)); // Wait 3 seconds before using the token
+			} else {
+				hasMorePages = false; // No more pages available
+			}
+
+			console.log('Total results: ' + allResults + 'END TOTAL RESULTS');
+		}
+
+		// Extract place IDs and fetch details for each place
+		const placeDetailsPromises = allResults.map((place) => {
+			return axios.get(
+				`https://maps.googleapis.com/maps/api/place/details/json`,
+				{
+					params: {
+						place_id: place.place_id,
+						key: apiKey,
+					},
+				}
+			);
+		});
+
+		// Fetch details for all places
+		const detailsResponses = await Promise.all(placeDetailsPromises);
+		const placesWithHours = detailsResponses.map((detailsResponse) => {
+			const details = detailsResponse.data.result;
+			console.log(details);
+			return {
+				name: details.name,
+				vicinity: details.vicinity,
+				rating: details.rating,
+				hours: details.opening_hours,
+				website: details.website,
+				location: details.geometry.location,
+			};
+		});
+
+		res.json(placesWithHours);
+	} catch (error) {
+		console.error('Error fetching data from Google Places API:', error.message);
+		res.status(500).send(error.message);
+	}
+});
+
+// Convert address to lat/lng using Google Geocoding API route
+app.get('/api/geocode', async (req, res) => {
+	const { address } = req.query;
+
+	const apiKey = 'AIzaSyD6CnVdH_CGjfZHfnksvGbL69aOw0HRvFw';
+
+	try {
+		// Call Google Geocoding API
+		const response = await axios.get(
+			'https://maps.googleapis.com/maps/api/geocode/json',
+			{
+				params: {
+					address: address,
+					key: apiKey,
+				},
+			}
+		);
+
+		const results = response.data.results;
+
+		if (results.length > 0) {
+			// Extract latitude and longitude from the response
+			const latLng = results[0].geometry.location;
+			res.json({ latLng }); // Send the lat/lng back to the client
+		} else {
+			res.status(404).json({ error: 'Address not found' });
+		}
+	} catch (error) {
+		console.error(
+			'Error fetching data from Google Geocoding API:',
+			error.message
+		);
+		res.status(500).json({ error: 'Failed to fetch location data' });
+	}
+});
+
+// Load Map
+app.get('/api/maps', async (req, res) => {
+	try {
+		const googleMapsApiKey = process.env.API_KEY; // Hide API key in environment variables
+
+		const googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&callback=initMap`;
+
+		res.send(googleMapsUrl); // Send the data as a response
+	} catch (error) {
+		console.error('Error fetching data from Google Maps API:', error.message);
+		res.status(500).send(error.message);
+	}
+});
+
+// Start the server
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
